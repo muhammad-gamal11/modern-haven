9708cb72b61e64975a5b735423875c239d984b7a
Muhammad Gamal <gamal9978@gmail.com>
2024-03-14 03:21:26 +0200

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -9,34 +9,23 @@ import {
   Error,
   About,
   Products,
-  PrivateRoute,
-  AuthWrapper,
 } from "./pages";
 function App() {
   return (
-    <AuthWrapper>
-      <Router>
-        <Navbar />
-        <Sidebar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:id" element={<SingleProduct />} />
-          <Route
-            path="checkout"
-            element={
-              <PrivateRoute>
-                <Checkout />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </AuthWrapper>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
